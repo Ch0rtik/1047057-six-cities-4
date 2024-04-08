2b853474528667dc80a6ddaff3d2511f4ec14589
Voronov Ivan <11optimus11@mail.ru>
2024-04-08 22:04:58 +0300

    fixed zoom

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -12,9 +12,9 @@ export enum AuthStatus {
 
 export const CITY_COORDINATES = new Map<City, Coordinates>([
   [City.Amsterdam, {lat: 52.3676, lng: 4.9041, zoom: 12}],
-  [City.Brussels, {lat: 50.8476, lng: 4.3572, zoom: 10}],
-  [City.Cologne, {lat: 50.9375, lng: 6.9603, zoom: 10}],
-  [City.Dusseldorf, {lat: 51.2230, lng: 6.7825, zoom: 10}],
-  [City.Hamburg, {lat: 53.5488, lng: 9.9872, zoom: 10}],
-  [City.Paris, {lat: 48.8566, lng: 2.3522, zoom: 10}],
+  [City.Brussels, {lat: 50.8476, lng: 4.3572, zoom: 12}],
+  [City.Cologne, {lat: 50.9375, lng: 6.9603, zoom: 12}],
+  [City.Dusseldorf, {lat: 51.2230, lng: 6.7825, zoom: 12}],
+  [City.Hamburg, {lat: 53.5488, lng: 9.9872, zoom: 12}],
+  [City.Paris, {lat: 48.8566, lng: 2.3522, zoom: 12}],
 ]);

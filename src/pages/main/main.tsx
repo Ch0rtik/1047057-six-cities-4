848bf6a9782ca38d164970735a5a6a0f5008fde1
Voronov Ivan <11optimus11@mail.ru>
2024-05-27 20:15:50 +0300

    Implemented city change

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -2,25 +2,37 @@ import PlacesList from '../../components/main-page/places-list/places-list.tsx';
 import PlacesSorting from '../../components/main-page/places-sorting/places-sorting.tsx';
 import Map from '../../components/map/map.tsx';
 import { useState } from 'react';
-import { useAppSelector } from '../../hooks/index.ts';
+import { useAppDispatch, useAppSelector } from '../../hooks/index.ts';
 import { OfferCardData, SortType, CityNames } from '../../types/types.ts';
 import sortOffers from '../../utils/utils.ts';
+import { changeCity } from '../../store/action.ts';
+import { CITY_COORDINATES } from '../../utils/const.ts';
 
 export default function Main() {
-  const [initialOffers, city] = useAppSelector((state) => [
-    [...state.offers].filter((offer: OfferCardData) => offer.city.name === state.city.name),
+  const [allOffers, city, sortType] = useAppSelector((state) => [
+    state.offers,
     state.city,
+    state.sortType
   ]);
+  const [initialOffers, setInitialOffer] = useState<OfferCardData[]>([...allOffers].filter((offer: OfferCardData) => offer.city.name === city.name));
   const placesFound = initialOffers.length;
 
   const [selectedOffer, setSelectedOffer] = useState<OfferCardData | undefined> (undefined);
   const [offers, setOffers] = useState<OfferCardData[]> ([...initialOffers]);
-  const onSortChange = (sortType: SortType) => {
-    setOffers(sortOffers(offers, sortType, initialOffers));
+  const onSortChange = (innerSortType: SortType) => {
+    setOffers(sortOffers(offers, innerSortType, initialOffers));
   };
   const handleListItemHover = (lsitItemId: string | undefined) => {
     const currentOffer = offers.find((offer) => offer.id.toString() === lsitItemId);
     setSelectedOffer(currentOffer);
+  };
+
+  const dispatch = useAppDispatch();
+  const generatehandleLocationItemClick = (cityName: CityNames) => () => {
+    dispatch(changeCity({name: cityName.toString(), location: CITY_COORDINATES.get(cityName)!}));
+    const newInitialOffers = [...allOffers].filter((offer: OfferCardData) => offer.city.name === cityName.toString());
+    setInitialOffer(newInitialOffers);
+    setOffers(sortOffers(offers, sortType, newInitialOffers));
   };
   return(
     <main className="page__main page__main--index">
@@ -29,32 +41,32 @@ export default function Main() {
         <section className="locations container">
           <ul className="locations__list tabs__list">
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Paris.toString()) ? ' tabs__item--active' : ''}`} href="#">
+              <a onClick={generatehandleLocationItemClick(CityNames.Paris)} className={`locations__item-link tabs__item${(city.name === CityNames.Paris.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Paris</span>
               </a>
             </li>
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Cologne.toString()) ? ' tabs__item--active' : ''}`} href="#">
+              <a onClick={generatehandleLocationItemClick(CityNames.Cologne)} className={`locations__item-link tabs__item${(city.name === CityNames.Cologne.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Cologne</span>
               </a>
             </li>
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Brussels.toString()) ? ' tabs__item--active' : ''}`} href="#">
+              <a onClick={generatehandleLocationItemClick(CityNames.Brussels)} className={`locations__item-link tabs__item${(city.name === CityNames.Brussels.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Brussels</span>
               </a>
             </li>
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Amsterdam.toString()) ? ' tabs__item--active' : ''}`}>
+              <a onClick={generatehandleLocationItemClick(CityNames.Amsterdam)} className={`locations__item-link tabs__item${(city.name === CityNames.Amsterdam.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Amsterdam</span>
               </a>
             </li>
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Hamburg.toString()) ? ' tabs__item--active' : ''}`} href="#">
+              <a onClick={generatehandleLocationItemClick(CityNames.Hamburg)} className={`locations__item-link tabs__item${(city.name === CityNames.Hamburg.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Hamburg</span>
               </a>
             </li>
             <li className="locations__item">
-              <a className={`locations__item-link tabs__item${(city.name === CityNames.Dusseldorf.toString()) ? ' tabs__item--active' : ''}`} href="#">
+              <a onClick={generatehandleLocationItemClick(CityNames.Dusseldorf)} className={`locations__item-link tabs__item${(city.name === CityNames.Dusseldorf.toString()) ? ' tabs__item--active' : ''}`}>
                 <span>Dusseldorf</span>
               </a>
             </li>

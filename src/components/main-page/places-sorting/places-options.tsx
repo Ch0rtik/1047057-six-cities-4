a2c05223d8ec8748968eddd69835be924d150ea6
Voronov Ivan <11optimus11@mail.ru>
2024-05-27 20:15:07 +0300

    Naming update

--- src/components/main-page/places-sorting/places-options.tsx
+++ src/components/main-page/places-sorting/places-options.tsx
@@ -4,19 +4,19 @@ import { useAppSelector } from '../../../hooks';
 
 type PlacesOptionsProps = {
   open: boolean;
-  onSortChoice: (sortType: SortType) => (evt: MouseEvent<HTMLLIElement>) => void;
+  generateHandlePlaceOptionClick: (sortType: SortType) => (evt: MouseEvent<HTMLLIElement>) => void;
 };
 
 
-export default function PlacesOptions({open, onSortChoice}: PlacesOptionsProps) {
+export default function PlacesOptions({open, generateHandlePlaceOptionClick}: PlacesOptionsProps) {
   const sortType = useAppSelector((state) => state.sortType);
 
   return(
     <ul className={`places__options places__options--custom${open ? ' places__options--opened' : ''}`}>
-      <li className={`places__option${sortType === SortType.Popular ? ' places__option--active' : ''}`} onClick={onSortChoice(SortType.Popular)} tabIndex={0}>Popular</li>
-      <li className={`places__option${sortType === SortType.LowToHigh ? ' places__option--active' : ''}`} onClick={onSortChoice(SortType.LowToHigh)} tabIndex={1}>Price: low to high</li>
-      <li className={`places__option${sortType === SortType.HighToLow ? ' places__option--active' : ''}`} onClick={onSortChoice(SortType.HighToLow)} tabIndex={2}>Price: high to low</li>
-      <li className={`places__option${sortType === SortType.TopRated ? ' places__option--active' : ''}`} onClick={onSortChoice(SortType.TopRated)} tabIndex={3}>Top rated first</li>
+      <li className={`places__option${sortType === SortType.Popular ? ' places__option--active' : ''}`} onClick={generateHandlePlaceOptionClick(SortType.Popular)} tabIndex={0}>Popular</li>
+      <li className={`places__option${sortType === SortType.LowToHigh ? ' places__option--active' : ''}`} onClick={generateHandlePlaceOptionClick(SortType.LowToHigh)} tabIndex={1}>Price: low to high</li>
+      <li className={`places__option${sortType === SortType.HighToLow ? ' places__option--active' : ''}`} onClick={generateHandlePlaceOptionClick(SortType.HighToLow)} tabIndex={2}>Price: high to low</li>
+      <li className={`places__option${sortType === SortType.TopRated ? ' places__option--active' : ''}`} onClick={generateHandlePlaceOptionClick(SortType.TopRated)} tabIndex={3}>Top rated first</li>
     </ul>
   );
 }

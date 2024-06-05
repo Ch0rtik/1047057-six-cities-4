50ad492efaa0e7f7583e08f31a6432f07de628af
Voronov Ivan <11optimus11@mail.ru>
2024-06-06 01:30:44 +0300

    Favorites Links

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import FavoritesList from '../../components/favorites-places/favorites-list';
 import { useAppSelector } from '../../hooks';
-import { OfferCardData } from '../../types/types';
+import { CityNames, OfferCardData } from '../../types/types';
+import { Link } from 'react-router-dom';
 
 export default function Favorites() {
   const offers: OfferCardData[] = useAppSelector((state) => state.offers);
@@ -22,20 +23,20 @@ export default function Favorites() {
           </div>
         </main>
         <footer className="footer container">
-          <a className="footer__logo-link" href="main.html">
+          <Link className="footer__logo-link" to='/'>
             <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-          </a>
+          </Link>
         </footer>
       </React.Fragment>
     );
   }
 
-  const parisOffers = favorites.filter((offer) => offer.city.name === 'Paris');
-  const cologneOffers = favorites.filter((offer) => offer.city.name === 'Cologne');
-  const brusselsOffers = favorites.filter((offer) => offer.city.name === 'Brussels');
-  const amsterdamOffers = favorites.filter((offer) => offer.city.name === 'Amsterdam');
-  const hamburgOffers = favorites.filter((offer) => offer.city.name === 'Hamburg');
-  const dusseldorfOffers = favorites.filter((offer) => offer.city.name === 'Dusseldorf');
+  const parisOffers = favorites.filter((offer) => offer.city.name === CityNames.Paris.toString());
+  const cologneOffers = favorites.filter((offer) => offer.city.name === CityNames.Cologne.toString());
+  const brusselsOffers = favorites.filter((offer) => offer.city.name === CityNames.Brussels.toString());
+  const amsterdamOffers = favorites.filter((offer) => offer.city.name === CityNames.Amsterdam.toString());
+  const hamburgOffers = favorites.filter((offer) => offer.city.name === CityNames.Hamburg.toString());
+  const dusseldorfOffers = favorites.filter((offer) => offer.city.name === CityNames.Dusseldorf.toString());
 
   return (
     <React.Fragment>
@@ -45,31 +46,31 @@ export default function Favorites() {
             <h1 className="favorites__title">Saved listing</h1>
             <ul className="favorites__list">
               {(amsterdamOffers.length > 0) && (
-                <FavoritesList cityName={'Amsterdam'} offers={amsterdamOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Amsterdam} offers={amsterdamOffers}></FavoritesList>
               )}
               {(brusselsOffers.length > 0) && (
-                <FavoritesList cityName={'Brussels'} offers={brusselsOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Amsterdam} offers={brusselsOffers}></FavoritesList>
               )}
               {(cologneOffers.length > 0) && (
-                <FavoritesList cityName={'Cologne'} offers={cologneOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Cologne} offers={cologneOffers}></FavoritesList>
               )}
               {(dusseldorfOffers.length > 0) && (
-                <FavoritesList cityName={'Dusseldorf'} offers={dusseldorfOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Dusseldorf} offers={dusseldorfOffers}></FavoritesList>
               )}
               {(hamburgOffers.length > 0) && (
-                <FavoritesList cityName={'Hamburg'} offers={hamburgOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Hamburg} offers={hamburgOffers}></FavoritesList>
               )}
               {(parisOffers.length > 0) && (
-                <FavoritesList cityName={'Paris'} offers={parisOffers}></FavoritesList>
+                <FavoritesList cityName={CityNames.Paris} offers={parisOffers}></FavoritesList>
               )}
             </ul>
           </section>
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
+        <Link className="footer__logo-link" to='/'>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
+        </Link>
       </footer>
     </React.Fragment>
   );

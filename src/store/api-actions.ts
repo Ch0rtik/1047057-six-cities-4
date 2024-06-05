1b3e60f36ee9a6081b1293d5df9224099e0db8ff
Voronov Ivan <11optimus11@mail.ru>
2024-06-06 01:04:14 +0300

    Error handling

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -8,11 +8,14 @@ import { dropToken, saveToken } from '../services/token';
 
 export const fetchOffers = createAsyncThunk<void, undefined, {dispatch: AppDispatch;state: State; extra: AxiosInstance}>(Action.FETCH_OFFERS,
   async (_arg, {dispatch, extra: api}) => {
-    dispatch(setOffersLoading(true));
-    const response = await api.get<OfferCardData[]>(APIRoute.Offers);
-    dispatch(setOffersLoading(false));
-    if (response.status === 200) {
+    try {
+      dispatch(setOffersLoading(true));
+      const response = await api.get<OfferCardData[]>(APIRoute.Offers);
       dispatch(loadOffers(response.data));
+    } catch {
+      dispatch(loadOffers([]));
+    } finally {
+      dispatch(setOffersLoading(false));
     }
   }
 );
@@ -35,58 +38,70 @@ export const fetchOfferPageData = createAsyncThunk<void, string, {dispatch: AppD
   async (offerId, {dispatch, extra: api}) => {
     dispatch(setOfferPageLoading(true));
 
-    const offerResponse = await api.get<OfferData>(`${APIRoute.Offers}/${offerId}`);
-    const reviewsResponse = await api.get<ReviewData[]>(`${APIRoute.Reviews}/${offerId}`);
-    const nearbyResponse = await api.get<OfferCardData[]>(`${APIRoute.Offers}/${offerId}${APIRoute.Nearby}`);
-    dispatch(setOfferPageLoading(false));
-    if (offerResponse.status === 200 && reviewsResponse.status === 200 && nearbyResponse.status === 200) {
+    try {
+      const offerResponse = await api.get<OfferData>(`${APIRoute.Offers}/${offerId}`);
+      const reviewsResponse = await api.get<ReviewData[]>(`${APIRoute.Reviews}/${offerId}`);
+      const nearbyResponse = await api.get<OfferCardData[]>(`${APIRoute.Offers}/${offerId}${APIRoute.Nearby}`);
+
       dispatch(loadOfferPage({offerData: offerResponse.data, reviewsData: reviewsResponse.data, nearbyData: nearbyResponse.data}));
+    } finally {
+      dispatch(setOfferPageLoading(false));
     }
   }
 );
 
 export const setFavoriteAction = createAsyncThunk<number, {id: string; status: number}, {dispatch: AppDispatch;state: State; extra: AxiosInstance}>(Action.SET_FAVORITE,
   async ({id, status }, {dispatch, extra: api}) => {
-    const response = await api.post<OfferCardData>(`${APIRoute.Favorite}/${id}/${status}`);
-    if (response.status === 201) {
-      dispatch(addFavorite(response.data));
-    } else if (response.status === 200) {
-      dispatch(removeFavoriteById(response.data.id));
+    try {
+      const response = await api.post<OfferCardData>(`${APIRoute.Favorite}/${id}/${status}`);
+      if (response.status === 201) {
+        dispatch(addFavorite(response.data));
+      } else if (response.status === 200) {
+        dispatch(removeFavoriteById(response.data.id));
+      }
+      return status;
+    } catch {
+      return -1;
     }
-    return status;
   }
 );
 
-export const sendCommentAction = createAsyncThunk<void, {newReviewData: NewReviewData; id: string}, {dispatch: AppDispatch;state: State; extra: AxiosInstance}>(Action.SEND_REVIEW,
+export const sendCommentAction = createAsyncThunk<boolean, {newReviewData: NewReviewData; id: string}, {dispatch: AppDispatch;state: State; extra: AxiosInstance}>(Action.SEND_REVIEW,
   async ({newReviewData, id}, {dispatch, extra: api}) => {
-    const response = await api.post<ReviewData>(`${APIRoute.Reviews}/${id}`, {comment: newReviewData.comment, rating: newReviewData.rating});
-    if (response.status === 201) {
+    try {
+      const response = await api.post<ReviewData>(`${APIRoute.Reviews}/${id}`, {comment: newReviewData.comment, rating: newReviewData.rating});
       dispatch(addReview(response.data));
+      return true;
+    } catch {
+      return false;
     }
   }
 );
 
 export const checkAuthAction = createAsyncThunk<void, undefined, {dispatch: AppDispatch; state: State; extra: AxiosInstance}>(Action.CHECK_AUTH,
   async(_arg, {dispatch, extra: api}) => {
-    const response = await api.get<UserData>(APIRoute.Login);
-    if (response.status === 200) {
+    try {
+      const response = await api.get<UserData>(APIRoute.Login);
       dispatch(requireAuthorization(AuthStatus.Auth));
       dispatch(addUser(response.data));
-    } else {
+    } catch {
       dispatch(requireAuthorization(AuthStatus.NoAuth));
     }
   }
 );
 
-export const loginAction = createAsyncThunk<void, AuthData, {dispatch: AppDispatch; state: State; extra: AxiosInstance}>(Action.LOG_IN,
+export const loginAction = createAsyncThunk<boolean, AuthData, {dispatch: AppDispatch; state: State; extra: AxiosInstance}>(Action.LOG_IN,
   async ({login: email, password}, {dispatch, extra: api}) => {
-    const response = await api.post<UserData>(APIRoute.Login, {email, password});
-    if (response.status === 200) {
+    try {
+      const response = await api.post<UserData>(APIRoute.Login, {email, password});
       saveToken(response.data.token);
       dispatch(requireAuthorization(AuthStatus.Auth));
       dispatch(addUser(response.data));
       dispatch(fetchFavorites());
       dispatch(fetchOffers());
+      return true;
+    } catch {
+      return false;
     }
   },
 );

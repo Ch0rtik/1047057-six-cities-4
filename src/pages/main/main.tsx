eaf8c2e30cd22b91215bbed92ec0b9afa8561330
Voronov Ivan <11optimus11@mail.ru>
2024-06-11 20:45:38 +0300

    Main Empty restructure

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -39,7 +39,7 @@ export default function Main() {
     setInitialOffer(newInitialOffers);
     setOffers(sortOffers(newInitialOffers, sortType, newInitialOffers));
   };
-  return (allOffers.length !== 0) ? (
+  return (
     <main className={`page__main page__main--index${offers.length === 0 ? ' page__main--index-empty' : ''}`}>
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
@@ -80,15 +80,7 @@ export default function Main() {
       </div>
       <div className="cities">
         {offers.length === 0 ? (
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">We could not find any property available at the moment in {city.name}</p>
-              </div>
-            </section>
-            <div className="cities__right-section"></div>
-          </div>
+          <MainEmpty city={city}></MainEmpty>
         ) : (
           <div className="cities__places-container container">
             <section className="cities__places places">
@@ -104,7 +96,5 @@ export default function Main() {
         )}
       </div>
     </main>
-  ) : (
-    <MainEmpty></MainEmpty>
   );
 }
